a506eae0e33c16c9b6411b0ee177fc9f4f39c788
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-03-18 21:46:46 +0500

    directory structure update

---
+++ Food Tracker/Assets/GameAssets/Scripts/Libs/Google/googleManager.cs
@@ -0,0 +1,58 @@
+using PlayFab;
+using System;
+
+#if !UNITY_IOS
+	using GooglePlayGames;
+	using PlayFab.ClientModels;
+	using UnityEngine;
+	using GooglePlayGames;
+	using GooglePlayGames.BasicApi;
+#else
+#endif
+
+public class googleManager
+{
+
+#if !UNITY_IOS
+    public googleManager()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        .AddOauthScope("profile")
+        .RequestServerAuthCode(false)
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    public void OnSignGmail(Action callbackSuccess, Action<PlayFabError> callbackFailure)
+    {
+        Social.localUser.Authenticate((bool success) => {
+            if (success)
+            {
+                var serverAuthCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+                PlayFabClientAPI.LoginWithGoogleAccount(new LoginWithGoogleAccountRequest()
+                {
+                    TitleId = "BF51B",
+                    ServerAuthCode = serverAuthCode, 
+                    CreateAccount = true
+                },
+                res =>
+                {
+                    callbackSuccess();
+                },
+                err =>
+                {
+                    callbackFailure(err);
+                });
+            }
+            else
+            {
+                callbackFailure(null);
+            }
+        });
+    }
+#else
+#endif
+}

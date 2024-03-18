8e04834054935630419e412c758d08fdbea5cf5d
Allauddin <allauddinraza0@gmail.com>
2024-03-18 06:00:40 +0500

    Singleton class playfab & Google

--- Food Tracker/Assets/Scripts/Login/loginController.cs
+++ Food Tracker/Assets/Scripts/Login/loginController.cs
@@ -7,15 +7,24 @@ using System.Collections;
 
 public class loginController : MonoBehaviour
 {
+    public static loginController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    [Header("Managers")]
+    public GmailManager gmailManager;
+    public PlayFabManager playFabManager;
     [Header("Utilities")]
     public GameObject UIBlocker;
     public GameObject RegisterUI;
     public GameObject LoginUI;
     public GameObject ForgotUI;
     public GameObject OnSignupGmail;
-    //public GameObject OnSignupApple;
-    //public GameObject video;
-    //public GameObject videosmall;
+  
 
 
     [Header("Login Screen")]
@@ -23,122 +32,35 @@ public class loginController : MonoBehaviour
     public TMP_InputField LoginPasswordField;
 
     [Header("Register Screen")]
-    //public TMP_InputField RegisterEmailFieldFirstName;
-    //public TMP_InputField RegisterEmailFieldLastName;
+   
     public TMP_InputField RegisterEmailField;
     public TMP_InputField RegisterPasswordwordField;
     public TMP_InputField ForgotEmailField;
 
-
-    private void Start()
-    {
-        playfabManager.Instance.OnServerInitialized();
-       
-
-        StartCoroutine(LoginRoutine());
-    }
-
-    private IEnumerator LoginRoutine()
-    {
-        yield return null; // Wait for one frame to allow the loader UI to update
-        bool usernameExists = PlayerPrefs.HasKey("username");
-        bool passwordExists = PlayerPrefs.HasKey("password");
-
-        if (usernameExists && passwordExists)
-        {
-            string username = PlayerPrefs.GetString("username");
-            string password = PlayerPrefs.GetString("password");
-
-            //video.SetActive(false);
-            //videosmall.SetActive(false);
-            PlayFabSettings.TitleId = "BF51B";
-            UIBlocker.SetActive(true);
-            playfabManager.Instance.OnTryLogin(username, password, callbackLoginSuccess, callbackLoginFailure);
-        }
-
-    }
-
-    //public void showVideo()
-    //{
-    //    #if !UNITY_IOS
-    //            //OnSignupApple.SetActive(false);
-    //    #else
-    //    #endif
-
-    //    #if UNITY_EDITOR
-    //            // Running in Unity Editor
-    //            //video.SetActive(true);
-    //    #elif UNITY_ANDROID
-    //        // Running on Android device
-    //        if (SystemInfo.deviceModel.Contains("pad") || SystemInfo.deviceModel.Contains("tablet"))
-    //        {
-    //            // Android tablet
-	   //             videosmall.SetActive(true);
-    //        }
-    //        else
-    //        {
-    //            // Android phone
-	   //             video.SetActive(true);
-    //        }
-    //    #elif UNITY_IOS
-    //        // Running on iOS device
-    //        if (SystemInfo.deviceModel.Contains("iPad"))
-    //        {
-    //            // iPad
-	   //             videosmall.SetActive(true);
-    //        }
-    //        else
-    //        {
-    //            // iPhone
-	   //             video.SetActive(true);
-    //        }
-    //    #else
-    //        // Unsupported platform
-	   //             videosmall.SetActive(true);
-    //    #endif
-    //}
-
     public void OnTryLogin()
     {
-        //SceneManager.LoadScene("gameplay");
-
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        string email = LoginEmailField.text;
-        string password = LoginPasswordField.text;
-        PlayFabSettings.TitleId = "BF51B";
-        UIBlocker.SetActive(true);
-        playfabManager.Instance.OnTryLogin(email, password, callbackLoginSuccess,callbackLoginFailure);
+        playFabManager.OnTryLogin();
     }
 
     public void OnPrivacyPolicy()
     {
-        Application.OpenURL("https://366degreefit.com/privacy-policy");
+        Application.OpenURL("");
     }
 
     public void OnOpenWebsite()
     {
-        Application.OpenURL("https://366degreefit.com");
+        Application.OpenURL("");
     }
 
     public void OnDummyLogin()
     {
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
+   
         Debug.LogError("Login succefull");
 }
 
 public void OnTryRegisterNewAccount()
     {
-        string email = RegisterEmailField.text;
-        string password = RegisterPasswordwordField.text;
-        //string fname = RegisterEmailFieldFirstName.text;
-        //string lname = RegisterEmailFieldLastName.text;
-
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        UIBlocker.SetActive(true);
-        playfabManager.Instance.OnTryRegisterNewAccount(email, password, callbackRegisterationSuccess, callbackRegisterationFailure);
+        playFabManager.OnTryRegisterNewAccount();
     }
 
     public void showForgotUI()
@@ -154,21 +76,10 @@ public void OnTryRegisterNewAccount()
 
     public void onForgotPassword()
     {
-        UIBlocker.SetActive(true);
-        string email = ForgotEmailField.text;
-        playfabManager.Instance.InitiatePasswordRecovery(email, callbackForgotSuccess, callbackForgotFailure);
+        playFabManager.onForgotPassword();
     }
 
-    public void callbackLoginSuccess(string email, string playfabID)
-    {
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        Popup.Show("LogIn Successful", "Welcome", "Dismiss");
-        Debug.Log("loginsuccesfull");
-        userSessionManager.Instance.initialize(email, playfabID);
-        UIBlocker.SetActive(false);
-    }
-
+  
     public void closeResetPassword()
     {
         ForgotUI.SetActive(false);
@@ -177,31 +88,12 @@ public void OnTryRegisterNewAccount()
 
     public void OnSignGmail()
     {
+        gmailManager.OnSignGmail();
+    }
+
    
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        UIBlocker.SetActive(true);
-        playfabManager.Instance.OnSignGmail(callbackGmailSuccess, callbackGmailFailure,callbackLoginSuccess, callbackLoginFailure);
-	
-    }
 
-    public void OnSignIOS()
-    {
-        #if UNITY_IOS
-                video.SetActive(false);
-                videosmall.SetActive(false);
-                   UIBlocker.SetActive(true);
-                   playfabManager.Instance.OnSignIOS(callbackGmailSuccess, callbackGmailFailure, callbackLoginSuccess, callbackLoginFailure);
-        #else
-        #endif
-    }
 
-    public void OnSignFacebook()
-    {
-        UIBlocker.SetActive(true);
-        Popup.Show("Password reset", "check your email address", "Dismiss");
-        UIBlocker.SetActive(false);
-    }
     public void Loginpanel()
     {
         RegisterUI.SetActive(false);
@@ -212,97 +104,7 @@ public void OnTryRegisterNewAccount()
         LoginUI.SetActive(false);
         RegisterUI.SetActive(true);
     }
-    public void callbackLoginFailure(PlayFabError error)
-    {
-        //showVideo();
-      
-        Popup.Show("Oops!", error.ErrorMessage, "Dismiss");
-        //RegisterUI.SetActive(true);
-        //LoginUI.SetActive(false);
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackRegisterationSuccess()
-    {
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        Debug.LogError("Registersuccesfull");
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackRegisterationFailure(PlayFabError error)
-    {
-        //showVideo();
-        Popup.Show("Oops!", error.ErrorMessage, "Dismiss");
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackGmailSuccess()
-    {
-        userSessionManager.Instance.initialize("gmail_session", "none");
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        Debug.LogError("loginsuccessfull");
-            UIBlocker.SetActive(false);
-    }
-
-    public void callbackGmailFailure(PlayFabError error)
-    {
-        //showVideo();
-        UIBlocker.SetActive(false);
-        if (error == null)
-        {
-            Popup.Show("Oops!", "Initialization Failed", "Dismiss");
-        }
-        else
-        {
-            Popup.Show("Oops!", error.ErrorMessage, "Dismiss");
-        }
-    }
-
-    public void callbackForgotSuccess()
-    {
-        LoginUI.SetActive(true);
-        ForgotUI.SetActive(false);
-        Popup.Show("Email Sent", "please check your email for further instruction", "Dismiss");
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackForgotFailure(PlayFabError error)
-    {
-        //showVideo();
-        UIBlocker.SetActive(false);
-        ForgotEmailField.text = "";
-        if (error == null)
-        {
-            Popup.Show("Oops!", "Initialization Failed", "Dismiss");
-        }
-        else
-        {
-            Popup.Show("Oops!", error.ErrorMessage, "Dismiss");
-        }
-    }
-
-    public void callbackFacebookInitialized()
-    {
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackFacebookSuccess()
-    {
-        //video.SetActive(false);
-        //videosmall.SetActive(false);
-        //SceneManager.LoadScene("gameplay");
-        userSessionManager.Instance.initialize("facebook_session", "none");
-        UIBlocker.SetActive(false);
-    }
-
-    public void callbackFacebookFailure(PlayFabError error)
-    {
-        //showVideo();
-        Popup.Show("Oops!", error.ErrorMessage, "Dismiss");
-        UIBlocker.SetActive(false);
-    }
+ 
 
     public void onRegisterUI()
     {
@@ -310,9 +112,6 @@ public void OnTryRegisterNewAccount()
         LoginUI.SetActive(false);
     }
 
-    void Update()
-    {
-
-    }
+ 
 
 }
